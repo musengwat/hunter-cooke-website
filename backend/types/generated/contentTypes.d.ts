a112eaf73dd4e5b242d2c2425c44bebb8f5e59a7
Thomas Musengwa <thomasmusengwa1@gmail.com>
2025-07-01 20:31:21 -0500

    :bug: make description field required in ApiPublicationCollectionPublicationCollection

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -608,7 +608,7 @@ export interface ApiPublicationCollectionPublicationCollection
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    description: Schema.Attribute.String;
+    description: Schema.Attribute.String & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
